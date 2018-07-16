79e1461c51defac6a9ea1bd17de53cea15b43740
cheangguan <32067821+cheangguan@users.noreply.github.com>
2018-07-16 22:32:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function maponclick() {
+    document.getElementById("inf").style.visibility = "visible";
+    document.getElementById("infh").innerHTML='ÕýéT´óÌÃ';
+}
+
+
+function infclick() {
+    document.getElementById("inf").style.visibility = "hidden";
+    }
