9ab5cd9431045411b864401ea8a5401cd7d79937
cheangguan <32067821+cheangguan@users.noreply.github.com>
2018-08-16 16:46:32 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function maponclick() {
-    document.getElementById("inf").style.visibility = "visible";
-    document.getElementById("infh").innerHTML='正門大堂';
-}
-
-
-function infclick() {
-    document.getElementById("inf").style.visibility = "hidden";
-    }
