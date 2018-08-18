290844a431c4263fb73cbd0897dbefc12497dd6c
cheangguan <32067821+cheangguan@users.noreply.github.com>
2018-08-18 11:24:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+document.write("<p>5</p>");
 $("map").css("height",($(window).height())-100)
 $("map").append("</br>");
 $("map").append(($(window).height()));
 $("map").append("</br>");
 $("map").append($("map").css("height"));
-document.write("<p>5</p>");
