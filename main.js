f8a5d2999b2956072d5726fb17c30d034523f51d
cheangguan <32067821+cheangguan@users.noreply.github.com>
2018-08-18 17:53:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var pageheight=document.documentElement.clientHeight;
-var mapheight=String(pageheight-100)+"px";
+var mapheight=String(pageheight-102)+"px";
 var mapheighttext=document.createTextNode(mapheight);
 document.getElementById("map").style.height=mapheight;
-document.getElementById("p1").appendChild(mapheighttext);
+document.getElementById("map").appendChild(mapheighttext);
