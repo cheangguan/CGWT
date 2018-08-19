3b9d268371c039e5b605eedd9742d9c24fddf85d
cheangguan <32067821+cheangguan@users.noreply.github.com>
2018-08-19 23:35:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+
+
 var pageheight=document.documentElement.clientHeight;
-var pagewidth=document.documentElement.clientWidth;
-
-
-
 var mapheight=String(pageheight-102)+"px";
 document.getElementById("map").style.height=mapheight;
 
 
-var righticon=String((pagewidth)-pagewidth*0.1)+"px";
-document.getElementById("getloc").style.left=righticon;
-document.getElementById("iot").style.left=righticon;
+var pagewidth=String((document.documentElement.clientWidth)-document.documentElement.clientWidth*0.1)+"px";
+document.getElementById("getloc").style.left=pagewidth;
+document.getElementById("iot").style.left=pagewidth;
+
+document.getElementById("toggle").onclick = function() 
+{
+  document.getElementById("sidebar").style.visibility="visible"; 
+  document.getElementById("sidebar").style.width=pagewidth;
+  document.getElementById("sidebar").style.height=String(pageheight-2)+"px";;                                   
+};
+
+document.getElementById("back").onclick = function() 
+{
+  document.getElementById("sidebar").style.visibility="hidden";                                 
+};
