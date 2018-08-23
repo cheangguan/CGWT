aeb3857e9eebc4078b3d4f54cec79bb6c99b64e6
cheangguan <32067821+cheangguan@users.noreply.github.com>
2018-08-24 01:44:45 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -14,13 +14,15 @@ var iconwidth=String(pagewidth-(pagewidth*0.1))+"px";
 
 document.getElementById("getloc").style.left=iconwidth;
 document.getElementById("iot").style.left=iconwidth;
+document.getElementById("lavel").style.left=iconwidth;
 
-
+document.getElementById("locinfo").style.top=String(pageheight-137)+"px";   
+document.getElementById("locinfo").style.width=String(pagewidth-2)+"px"; 
 document.getElementById("toggle").onclick = function() 
 {
   document.getElementById("sidebar").style.visibility="visible"; 
   document.getElementById("sidebar").style.width=iconwidth;
-  document.getElementById("sidebar").style.height=String(pageheight-2)+"px";;   
+  document.getElementById("sidebar").style.height=String(pageheight-2)+"px";   
 document.getElementById("functionbar").style.visibility="hidden";                                  
 };
 
@@ -38,3 +40,35 @@ document.getElementById("functionbar").style.height=String(pageheight-pageheight
 document.getElementById("functionbar").style.top=String(pageheight*0.3-2)+"px";; 
 document.getElementById("sidebar").style.visibility="hidden";         
 };
+
+document.getElementById("getloc").onclick = function() 
+{
+
+document.getElementById('map').scrollTop =1;
+document.getElementById('map').scrollLeft =120;
+document.getElementById('mapf').src="GFdw.png";
+document.getElementById("locinfo").style.visibility="visible"; 
+};
+
+document.getElementById("bf").onclick = function() 
+{
+
+
+document.getElementById('mapf').src="UF.png";
+
+};
+document.getElementById("gf").onclick = function() 
+{
+
+
+document.getElementById('mapf').src="GFdw.png";
+
+};
+document.getElementById("1f").onclick = function() 
+{
+
+
+document.getElementById('mapf').src="1F.png";
+
+};
+
